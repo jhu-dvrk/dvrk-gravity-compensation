e1d23d4b9359e9436ed24b7475f61257012aec36
Anton Deguet <anton.deguet@jhu.edu>
2019-01-02 11:22:39 -0500

    gc_controller: minor fixes, typo, messages

--- gc_controller.m
+++ gc_controller.m
@@ -1,4 +1,4 @@
-function mtm_gc_controller = gc_controller(gc_controller_cofig_json)
+function mtm_gc_controller = gc_controller(gc_controller_config_json)
     %  Author(s):  Hongbin LIN, Vincent Hui, Samuel Au
     %  Created on: 2018-10-05
     %  Copyright (c)  2018, The Chinese University of Hong Kong
@@ -10,12 +10,12 @@ function mtm_gc_controller = gc_controller(gc_controller_cofig_json)
     %                        ARM_data_path_with_date);
 
     % Checking arguments
-    argument_checking(gc_controller_cofig_json);
+    argument_checking(gc_controller_config_json);
 
-    % Reading config file "gc_controller_cofig_json"
-    fid = fopen(gc_controller_cofig_json);
+    % Reading config file "gc_controller_config_json"
+    fid = fopen(gc_controller_config_json);
     if fid<3
-        error('Cannot find file %s',gc_controller_cofig_json)
+        error('Cannot find file %s', gc_controller_config_json)
     end
     raw = fread(fid, inf);
     str = char(raw');
@@ -24,7 +24,7 @@ function mtm_gc_controller = gc_controller(gc_controller_cofig_json)
 
     % Version Checking
     if config.version ~= '1.0'
-        error('The version of dvrk gravity compensation should be 1.0')
+        error('The version of dVRK gravity compensation should be 1.0')
     end
 
     % General Setting
@@ -67,13 +67,13 @@ function mtm_gc_controller = gc_controller(gc_controller_cofig_json)
 
 end
 
-function argument_checking(gc_controller_cofig_json)
-    if  ~ischar(gc_controller_cofig_json)
-        error('Input argument "gc_controller_cofig_json" should be a char arrary')
+function argument_checking(gc_controller_config_json)
+    if  ~ischar(gc_controller_config_json)
+        error('Input argument "gc_controller_config_json" should be a char array')
     end
-    if ~(strcmp(gc_controller_cofig_json(end-4:end),'.json'))
-        sprintf(['Input of argument ''gc_controller_cofig_json''= %s',...
-            ' is error, you should input file with .json extend format '],gc_controller_cofig_json);
+    if ~(strcmp(gc_controller_config_json(end-4:end),'.json'))
+        sprintf(['Input of argument ''gc_controller_config_json''= %s',...
+            ' is error, you should input file with .json extension '], gc_controller_config_json);
     end
 end
 
